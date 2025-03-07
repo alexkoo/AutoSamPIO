3bbfc021b55aab989e70ee8d8c3136f3865ec29c
alexkoo <kozlovnxt@yandex.ru>
2025-03-07 14:36:33 +0300

    переезд на GyverDS18.h

--- AutoSam/header.h
+++ AutoSam/header.h
@@ -3,16 +3,29 @@
 //**************************************************************************************************// LIB
 //#include <Arduino.h>
 //#include <ESP8266SSDP.h>      //esp8266 core убран в исходнике
+//#include <ESP8266SSDP.h>      //esp8266 core убран в исходнике
 //#include <DNSServer.h>        //esp8266 core  https://github.com/esp8266/Arduino
+//#include <ESP8266mDNS.h>       //esp8266 core
+//#include <WiFiUdp.h>           //esp8266 core
 //#include <ESP8266mDNS.h>       //esp8266 core
 //#include <WiFiUdp.h>           //esp8266 core
 //#include <Wire.h>             //esp8266 core i2c
 //#include <SPI.h>              //esp8266 core, нужен в platformio для BMx280I2C
 //#include <ESP8266WiFi.h>      //https://github.com/esp8266/Arduino
 //#include <FS.h>               //esp8266 core
+//#include <ESP8266WiFi.h>      //https://github.com/esp8266/Arduino
+//#include <FS.h>               //esp8266 core
 //#include <WString.h> //esp8266 core
 // #include <OneWire.h>           //https://github.com/PaulStoffregen/OneWire
 // #include <DallasTemperature.h> //https://github.com/milesburton/Arduino-Temperature-Control-Library
+
+#include <ESP8266WebServer.h> //esp8266 core
+
+#include <EEPROM.h>           //esp8266 core
+#include <LittleFS.h> // //esp8266 core 
+#include <ArduinoOTA.h>        //esp8266 core
+#include <WiFiManager.h>      //https://github.com/tzapu/WiFiManager
+#include <LiquidCrystal_I2C.h> //https://github.com/marcoschwartz/LiquidCrystal_I2C.git
 
 #include <ESP8266WebServer.h> //esp8266 core
 
@@ -28,9 +41,22 @@
 #include <GyverDS18Array.h>     // https://github.com/GyverLibs/GyverDS18
 //#include <microDS18B20.h> //https://github.com/GyverLibs/microDS18B20
 //#define DS_CRC_USE_TABLE true  // Использовать таблицу для CRC. Быстрее, но +256 байт flash (<1мкс VS ~6мкс) (умолч. false)microDS18B20.h> microDS18B20.h>
+#include <GyverFilters.h>      //https://alexgyver.ru/gyverfilters/
+
+#include <GyverDS18.h>      // https://github.com/GyverLibs/GyverDS18
+#include <GyverDS18Array.h>     // https://github.com/GyverLibs/GyverDS18
+//#include <microDS18B20.h> //https://github.com/GyverLibs/microDS18B20
+//#define DS_CRC_USE_TABLE true  // Использовать таблицу для CRC. Быстрее, но +256 байт flash (<1мкс VS ~6мкс) (умолч. false)microDS18B20.h> microDS18B20.h>
 
 #include <GyverNTP.h>
 GyverNTP ntp(3);
+
+
+
+
+
+
+
 
 
 
@@ -67,6 +93,7 @@ WiFiClient telnet;
 
 
 
+
 /*
 uint8_t sensor_address[][8] = {
     // массив адресов датчиков SteamSensor, PipeSensor, WaterSensor, TankSensor;
@@ -86,9 +113,6 @@ uint64_t ds_address[] = {     // массив адресов датчиков St
 };
 GyverDS18Array ds_sensors(ds_pin, ds_address, 4);
 GyverDS18Single ds_single(ds_pin);  
-bool setResolution(12);
-
-// uint16_t getConversionTime();  // получить текущее время измерения температуры, мс
 
 
 uint8_t steam_sensor_num = 0;
